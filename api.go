3277b4ac5c51b1399b2a1170c514e23f7a790263
Alexander Baranov <a.baranov@protonmail.com>
2023-02-04 11:50:43 +0400

    remove code duplication

--- api.go
+++ api.go
@@ -1,48 +1,15 @@
 package kagi
 
 import (
-	"bytes"
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 const apiEndpoint = "https://labs.kagi.com/v1"
 
 func apiURL(suffix string) string {
 	return fmt.Sprintf("%s%s", apiEndpoint, suffix)
-}
-
-func GetTakeways(ctx context.Context, url string) (response TakeawaysResponse, err error) {
-	var reqBytes []byte
-	reqBytes, err = json.Marshal(map[string]string{
-		"url": url,
-	})
-	if err != nil {
-		return
-	}
-
-	req, err := http.NewRequest("POST", apiURL("/takeaways"), bytes.NewBuffer(reqBytes))
-	if err != nil {
-		return
-	}
-
-	req = req.WithContext(ctx)
-	for i := 0; i < 20; i++ {
-		err = sendRequest(req, &response)
-		if err != nil {
-			return
-		}
-
-		if response.Status == "completed" {
-			return
-		}
-
-		time.Sleep(2 * time.Second)
-	}
-	return
 }
 
 func sendRequest(req *http.Request, v interface{}) (err error) {
